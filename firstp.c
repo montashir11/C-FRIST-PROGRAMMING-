4f9df3f52d650107c2a73e14e23f89196520ca4d
MD : MONTASHIR MOBINE RIFAT <117707301+montashir11@users.noreply.github.com>
2023-02-04 16:57:33 +0600

    Add files via upload

---
+++ firstp.c
@@ -0,0 +1,15 @@
+//start
+
+#include<stdio.h>
+int main()
+{
+    printf("md montashir mobine rifat\n");//Using Esacape sequence
+    printf("ccn university\n");
+    printf("\a018741788858");
+    getch() ;
+
+}
+/*programer name : md hacker
+DATE : 2/3/2023
+*/
+
